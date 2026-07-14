26c8bcaf3856ecf4d1b6814fc37fdf7d31b5851d
Tarun Joshi <joshitarun22feb@gmail.com>
2026-07-14 18:24:14 +0530

    Multitask Project weatherUpdate_2

---
+++ Assignment/Assignment_9/script4.js
@@ -0,0 +1,71 @@
+const dailyGoals = document.querySelector(".dailyGoals");
+const dailyGoal = document.querySelector(".dailyGoal");
+const closeDailyGoal = document.querySelector("#closeDailyGoal");
+const addnewGoal = document.querySelector("#addnewGoal");
+const goalsForm = document.querySelector(".goalsForm");
+const closeCreate = document.querySelector("#closeCreate");
+const goalcreateForm = document.querySelector("#goalcreateForm");
+const goalsCard = document.querySelector(".goalsCard");
+
+dailyGoal.addEventListener("click", () => {
+  dailyGoals.style.display = "flex";
+});
+closeDailyGoal.addEventListener("click", () => {
+  dailyGoals.style.display = "none";
+});
+
+addnewGoal.addEventListener("click", () => {
+  goalsForm.style.display = "flex";
+});
+
+closeCreate.addEventListener("click", (e) => {
+  e.preventDefault();
+  goalsForm.style.display = "none";
+});
+
+let goalData = JSON.parse(localStorage.getItem("Goals")) || [];
+
+let displayGoal = function () {
+  goalsCard.innerHTML = "";
+  goalData.forEach((elem, index) => {
+    goalsCard.innerHTML += `<div class="dailyGoalsCard">
+            <h1 id="goalCardHead">${elem.head}</h1>
+            <p id="goalCardDesc">${elem.description}</p>
+            <div class="cardbutton">
+              <button id="goalComplete">Completed</button>
+              <button onclick="deleteGoal(${elem.id})" id="goalDelete">Delete</button>
+            </div>
+          </div>`;
+  });
+};
+displayGoal();
+
+let goalFunction = (event) => {
+  let head = event.target[0].value;
+  let description = event.target[1].value;
+  if (head.trim() === "" || description.trim() === "") {
+    return;
+  }
+  let obj = {
+    id: Math.floor(Math.random() * 10),
+    head,
+    description,
+  };
+  goalData.push(obj);
+  localStorage.setItem("Goals", JSON.stringify(goalData));
+};
+
+goalcreateForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  goalFunction(event);
+  displayGoal();
+  goalcreateForm.reset();
+  goalsForm.style.display = "none";
+});
+
+const deleteGoal = (delete_id) => {
+ goalData = goalData.filter(goal => goal.id !== delete_id);
+  localStorage.setItem("Goals", JSON.stringify(goalData));
+
+  displayGoal();
+};
