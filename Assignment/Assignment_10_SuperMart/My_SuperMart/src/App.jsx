633c73996921fcb78afd438d23fc7828942756e1
Tarun Joshi <joshitarun22feb@gmail.com>
2026-07-27 23:53:04 +0530

    My_SuperMart

--- Assignment/Assignment_10_SuperMart/My_SuperMart/src/App.jsx
+++ Assignment/Assignment_10_SuperMart/My_SuperMart/src/App.jsx
@@ -1,9 +1,5 @@
 import React from 'react'
-import Login from './Components/login'
-import Register from './Components/Register'
 import AppRoutes from './Routes/AppRoutes'
-import Cart from './Components/Cart'
-
 const App = () => {
 
 
